fe9b918b45cb83c7daba757464320e54ec394802
claudiamastan <claudiachen97@gmail.com>
2019-04-03 19:53:04 -0700

    initial push, set up login + regist page, to do: set up & connect  db

---
+++ project1/application.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import requests
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL iset s not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        name = request.form.get("name")
+        uname = request.form.get("uname")
+        password = request.form.get("password")
+        print(name,uname,password)
+        return render_template("main.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        uname = request.form.get("uname")
+        password = request.form.get("password")
+        print(name,uname,password)
+        return render_template("main.html")
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
